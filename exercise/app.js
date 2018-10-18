2dce237fa68e27156ed1fef92693f648231f7102
Vinston Guillaume <vinstonguillaume@gmail.com>
2018-10-18 02:32:56 -0400

    Added Dockerfile

--- exercise/app.js
+++ exercise/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 // Database
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/exercise');
+var db = monk('dev.jabaridash.com:27017');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
