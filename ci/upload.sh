9dd7ded293fc19756f3c5e794464d2ddb16e8dbc
Vinston Guillaume <vinstonguillaume@gmail.com>
2018-11-29 13:30:31 -0500

    docker push

--- ci/upload.sh
+++ ci/upload.sh
@@ -58,8 +58,8 @@ curl --data "$(generate_post_data)" "https://api.github.com/repos/$repo_full_nam
 
 # Build docker images with the same version number
 echo "Building docker images"
-docker build -t vguillaume/lets-exercise:$version .
-docker build -t vguillaume/lets-exercise-client:$version .
+docker build -t vguillaume/lets-exercise:$version  -f ../server/Dockerfile .
+docker build -t vguillaume/lets-exercise-client:$version  -f ../client/Dockerfile /
 
 # Push them to docker hub
 echo "Pushing docker images"
